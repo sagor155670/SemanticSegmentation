31cac54e7b02cdbb2f07d8b6df7440aff6051fad
sagor155670 <sagor@braincraftapps.com>
2024-03-11 16:50:59 +0600

    Initial Commit

---
+++ SemanticSegmentation/SemanticSegmentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SemanticSegmentationApp.swift
+//  SemanticSegmentation
+//
+//  Created by Saiful Islam Sagor on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SemanticSegmentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
